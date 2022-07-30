543d5275f3ba9c4badba0790833cabbd1b038afe
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-07-30 16:40:26 +0200

    initial commits

---
+++ src/main/java/com/jdev/soap/hrms/employeemanagementsys/EmployeeManagementsysApplication.java
@@ -0,0 +1,15 @@
+package com.jdev.soap.hrms.employeemanagementsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementsysApplication.class, args);
+        System.out.println("Hello world");
+
+    }
+
+}
---
+++ src/test/java/com/jdev/soap/hrms/employeemanagementsys/EmployeeManagementsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdev.soap.hrms.employeemanagementsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
